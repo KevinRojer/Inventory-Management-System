a268c0a1d2afa705a63844749f911870bf8a5329
Kevin Rojer <kevin.rojer@live.com>
2024-01-11 00:12:25 -0400

    Update main.py
    Created initial layout

--- app/main.py
+++ app/main.py
@@ -6,37 +6,27 @@ Created on Thu Nov 30 22:14:19 2023
 @author: kevinrojer
 """
 
-
-import dash
+from dash import html, dcc
 import dash_bootstrap_components as dbc
-from dash import dcc, html
-from dash.dependencies import Input, Output
-from dash.exceptions import PreventUpdate
 from app import app
-import auth
 import config
+from flask_login import current_user, login_user
+from components.navbar import headerbar
+from components.footer import footer
 
 
 app.layout = dbc.Container([
-    # Nav bar
-    html.Div([], id="nav-bar"),
-    
-    # Dynamic Page content
-    dcc.Location(id='url', refresh=True),
+    # Handle URL
+    dcc.Location(id="url", refresh=False),
+    #Header
+    headerbar,
+    # Dynamic page content
     html.Div(id="page-content"),
-    
-    # Footer
-    html.Dic([], id="footer-content"),
+    #footer
+    footer,
     ])
+    
 
-@callback(Output('page-content', 'children'),
-          [Input('url', 'pathname')])
-def display_page(pathname):
-    # Redirect to login page if not logged in
-    if pathname == '/login' or pathname == '/':
-        return auth.login_layout
-    else:
-        # Logic to display other pages after successful login
-        # This can be your home page or other content pages
-        # e.g., return home_layout if pathname == '/home'
-        pass
+
+if __name__ == "__main__":
+    app.run_server(debug=config.DEBUG, dev_tools_props_check=config.DEBUG)
