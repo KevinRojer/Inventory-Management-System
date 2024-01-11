046a4c4c245b970f5fe1bd80ac95a8110471dee4
Kevin Rojer <kevin.rojer@live.com>
2024-01-11 00:11:17 -0400

    Update app.py
    Configured the app

--- app/app.py
+++ app/app.py
@@ -9,9 +9,18 @@ Created on Thu Nov 30 20:46:24 2023
 import dash
 import dash_bootstrap_components as dbc
 import config
+from flask import Flask
 
+
+# Set up Flask
+server = Flask(__name__)
 
 # Initialize the Dashly Application:
 app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.BOOTSTRAP, config.APP_STYLE])
+                external_stylesheets=[dbc.themes.BOOTSTRAP, config.APP_STYLE],
+                server=server,
+                suppress_callback_exceptions=True)
 app.title = config.APP_TITLE
+
+# Configure Flask Server
+server.config.update(SECRET_KEY=config.SECRET_KEY)
