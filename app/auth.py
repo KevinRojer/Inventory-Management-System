260e7c5e441a375ad7ad7313f6fd9dadaceb2d7b
Kevin Rojer <kevin.rojer@live.com>
2024-01-11 00:12:11 -0400

    Update auth.py
    Added user class

--- app/auth.py
+++ app/auth.py
@@ -7,15 +7,30 @@ Created on Thu Nov 30 22:23:50 2023
 """
 
 
-import dash
-import dash_bootstrap_components as dbc
-from dash import dcc, html
-from dash.dependencies import Input, Output
-from dash.exceptions import PreventUpdate
 from app import app
-from ui import login
-import auth
+from flask_login import LoginManager, UserMixin
 import config
 
 
-login_layout = login.layout
+# Initialize login manager
+login_manager = LoginManager()
+login_manager.init_app(app.server)
+login_manager.login_view = "/loign"
+
+
+# User Class
+class User(UserMixin):
+    
+    def __init__(self, username):
+        self.id = username
+        
+        
+@login_manager.user_loader
+def user_loader(user_id):
+    return User(user_id)
+
+
+def validate_user(username, password):
+    if (username in config.users) and (password == config.users[password]):
+        return True
+    return False
